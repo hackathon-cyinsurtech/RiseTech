9d3a739b512ac4ff310dfd0e23fd6c6860c8898c
tziocy <tzio@epicode.gr>
2018-01-27 14:30:32 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+
+var app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+var db = JSON.parse(fs.readFileSync('./db.json', 'utf8'))
+
+//  Post Rates
+app.post('/rates', function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  res.send(db)
+})
+
+app.listen(3000, () => console.log( 'Start Listening to 3000'))
