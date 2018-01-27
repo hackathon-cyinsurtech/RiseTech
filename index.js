6ff151f4da12841cd909882c496507276bfcfee4
tziocy <tzio@epicode.gr>
2018-01-27 14:45:59 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const fs = require('fs')
+const app = express()
 
-var app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-var db = JSON.parse(fs.readFileSync('./db.json', 'utf8'))
-
-//  Post Rates
-app.post('/rates', function (req, res) {
-  if (!req.body) return res.sendStatus(400)
-  res.send(db)
-})
-
-app.get('/', function (req, res) {
-    if (!req.body) return res.sendStatus(400)
-    res.send({'test': 'test'})
-})
-app.listen(3000, () => console.log( 'Start Listening to 3000'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
