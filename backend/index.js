5474a16f4faca394510bc2cd8c9c94534ef91f44
tziocy <tzio@epicode.gr>
2018-01-28 17:13:24 +0200

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const fs = require('fs')
 const faker = require('faker')
 const mongoose = require('mongoose')
+const expressValidator = require('express-validator');
 
 const app = express()
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://congrats-people-tzio.c9users.io:8081')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-auth')
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    if (req.method === "OPTIONS") 
-      res.status(200).send()
-    else 
-      next()
-})
-app.use(bodyParser.json())
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-auth');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
-mongoose.connect('bananash:bananash123@ds117178.mlab.com:17178/bananash')
-mongoose.Promise = global.Promise
+
+app.use(bodyParser.json())
+app.use(expressValidator());
+
+mongoose.connect('mongodb://bananash:bananash123@ds117178.mlab.com:17178/bananash')
+mongoose.Promise = global.Promise;
 
 //Get the default connection
 var db = mongoose.connection;
-var db = JSON.parse(fs.readFileSync('./db.json', 'utf8'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 const quotation = require('./routes/quotation')
 const customer = require('./routes/customer')
+const company = require('./routes/company')
+const chatfuel = require('./routes/chatfuel')
 
 app.use(quotation)
 app.use(customer)
+app.use(company)
+app.use(chatfuel)
 
 app.listen(3000, () => console.log( 'Start Listening to 3000'))
